529716a9b2f9d342666c2f48a4fd56102749c2cf
MichaelJuannn <juan600d@gmail.com>
2023-12-30 11:20:51 +0700

    add file upload

--- resources/views/book/book.blade.php
+++ resources/views/book/book.blade.php
@@ -1,9 +1,9 @@
-<form action="/dashboard/books" method="post">
+<form action="/dashboard/books" method="post" enctype="multipart/form-data">
     <input type="text" name="title">
     <input type="text" name="category">
     <input type="text" name="description">
     <input type="text" name="quantity">
-    <input type="text" name="file">
-    <input type="text" name="cover">
+    <input type="file" name="file">
+    <input type="file" name="cover">
     <button type="submit">submit</button>
 </form>
